6f771faa85bfab99b8f32f4f1798b59e4169d8ce
eulogy4me <backy42@nate.com>
2025-02-09 10:42:19 +0000

    Done

--- ML.py
+++ ML.py
@@ -3,9 +3,9 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def get_pred(df:pd.DataFrame, ModelTrainer, targets:str):
+def get_pred(df:pd.DataFrame, MODEL, targets:str):
     X = df.drop(columns=targets)
-    y_pred = ModelTrainer.model.evaluate_model(X, None)
+    y_pred = MODEL.evaluate(X, None)
     return y_pred
 
 def preprocess(df:pd.DataFrame, targets:str):
@@ -38,11 +38,20 @@ if __name__ == "__main__":
     targets = ['Qty']
     X, y , X_test, y_test, df_temp = preprocess(df, targets)
 
-    QTY = models.Qty.ModelTrainer()
-    QTY.load(path + "/pkl/qty.cbm")
-    pred_results = get_pred(df_temp, QTY, targets)
+    MODEL = models.Single.Model()
+    # param_grid = {
+    #     'iterations': (100, 1500, 500),
+    #     'depth': (4, 12, 2),
+    #     'learning_rate': (0.1, 0.001, 0.01),
+    #     'l2_leaf_reg': (2, 10, 2),
+    #     'bagging_temperature': (1, 3, 1),
+    #     'random_strength': (1, 5, 1)
+    # }
+    # MODEL.train(X,y,param_grid)
+    # MODEL.save(path + "/pkl/qty.cbm")
+    MODEL.load(path + "/pkl/qty.cbm")
+    pred_results = get_pred(df_temp, MODEL, targets)
     df[targets] = pred_results
     df[targets] = pd.to_numeric(df[targets], errors='coerce').astype(int)
-
-
+    
     df.to_csv(path + "/rslt/output.csv")
